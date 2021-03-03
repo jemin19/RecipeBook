50a0a585eb4bf286bcd65a0d177eed60d11bdf93
Jemin Patel <jemin.patel19@gmail.com>
2021-03-02 22:19:02 -0500

    observables are working as expected

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -6,20 +6,20 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
+  
 
   private recipes: Recipe[] = [
     new Recipe(
       'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      'https://www.lilvienna.com/wp-content/uploads/Recipe-Viennese-Pork-Schnitzel.jpg',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe('Big Fat Burger',
       'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      'https://www.cfacdn.com/img/order/COM/Menu_Refresh/Entree/Entree%20PDP/_0000s_0013_%5BFeed%5D_0001s_0022_Entrees_Spicy-Grilled-Sandwich.png',
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
@@ -30,6 +30,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(id: number){
+    return this.recipes.slice()[id]; 
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
